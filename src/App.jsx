948da79c25a4f87198f70db48c4faa281aee994a
grace <gracebouanga28@gmail.com>
2024-01-17 16:53:56 +0100

    new feature

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
-      <p className="text-blue-600  bg-violet-900">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+      <p className="text-blue-600  bg-violet-900">Lorem Ipsum is non non simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
     </div>
 
     </>
