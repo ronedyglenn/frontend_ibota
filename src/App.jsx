4abc187c1bb8e6f9327e06df7fbb220399639b66
grace <gracebouanga28@gmail.com>
2024-01-17 15:20:42 +0100

    reversion

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 export default function App() {
+
+
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <div className="flex flex-col">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <p className="text-blue-600  bg-violet-900">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+    </div>
+
+    </>
   )
 }
